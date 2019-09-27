a491c3181009b8f81a4f5dc3eace80b3d8bf4f93
Christian Dimitrov <christian_dimitrov@abv.bg>
2019-09-27 12:26:18 +0300

    basic client-side validation added

--- src/components/Courses/ManageCoursePage.js
+++ src/components/Courses/ManageCoursePage.js
@@ -43,8 +43,19 @@ function ManageCoursePage({
     }));
   }
 
+  function formIsValid() {
+    const { title, authorId, category } = course;
+    const errors = {};
+    if (!title) errors.title = "Title is required";
+    if (!authorId) errors.author = "Author is required";
+    if (!category) errors.category = "Category is required";
+    setErrors(errors);
+    return Object.keys(errors).length === 0;
+  }
+
   function handleSave(e) {
     e.preventDefault();
+    if (!formIsValid()) return;
     setSaving(true);
     saveCourse(course)
       .then(() => {
